2eb633d6a99b5fa6ad8f5ccfe5fc5e5b0b4decb9
olivier <olivier.winter@hotmail.fr>
2024-02-01 09:34:19 +0000

    documentation: agea volumes examples

--- examples/atlas_genomics_agea_regions_predictor.py
+++ examples/atlas_genomics_agea_regions_predictor.py
@@ -1,21 +1,25 @@
+"""
+In this example we train a neural network to predict the cosmos regions from the gene expression
+This demonstrates how to align and sample brain regions relative to the gene expression volumes.
+
+This examples requires `sklearn` and `seaborn` to be installed on top of the `iblatlas` requirements.
+"""
+
+import numpy as np
 import matplotlib.pyplot as plt
+
+import seaborn as sns
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import sklearn.metrics
+
 from iblatlas.genomics import agea
 
+# %% Load the agea atlas
 df_genes, gene_expression_volumes, atlas_agea = agea.load()
 
-igenes = (0,)
-fig, axs = plt.subplots(3, 2, sharex=True, sharey=True)
-atlas_agea.plot_cslice(0, ax=axs[0, 0])
-atlas_agea.plot_cslice(0, ax=axs[1, 0], volume='annotation')
-atlas_agea.plot_cslice(0, ax=axs[2, 0], volume=gene_expression_volumes[igenes[0]], cmap='viridis')
-atlas_agea.plot_sslice(0, ax=axs[0, 1])
-atlas_agea.plot_sslice(0, ax=axs[1, 1], volume='annotation')
-atlas_agea.plot_sslice(0, ax=axs[2, 1], volume=gene_expression_volumes[igenes[0]], cmap='viridis')
-fig.tight_layout()
-
-
 # %% remap the the agea atlas at the cosmos level parcellation
-import numpy as np
 ne = gene_expression_volumes.shape[0]
 sel = atlas_agea.label.flatten() != 0  # remove void voxels
 # reshape in a big array nexp x nvoxels this takes a little while
@@ -24,10 +28,6 @@ aids = atlas_agea.regions.id[atlas_agea.label.flatten()[sel]]
 aids_cosmos = atlas_agea.regions.remap(aids, 'Allen-lr', 'Cosmos')
 
 # %% now we learn to predict the cosmos labels from the gene expression
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-import sklearn.metrics
 X_train, X_test, y_train, y_test = train_test_split(gexps, aids)
 scaler = StandardScaler()
 scaler.fit(gexps)
@@ -40,7 +40,6 @@ clf.score(X_test, y_test)
 classes_labels = atlas_agea.regions.id2acronym(clf.classes_)
 
 # %% Plot the confusion matrix
-import seaborn as sns
 cm = sklearn.metrics.confusion_matrix(y_test, clf.predict(X_test), normalize='pred')
 sklearn.metrics.ConfusionMatrixDisplay(cm, display_labels=classes_labels).plot(ax=plt.gca(), cmap='magma')
 
